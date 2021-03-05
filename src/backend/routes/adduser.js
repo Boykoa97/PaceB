0f2cfbc46c479149c63dc795ae2b854894713f59
cpasteris <cpasteris@shaw.ca>
2021-03-04 20:07:20 -0700

    signup and database altered to allow for the new style of skill list

--- src/backend/routes/adduser.js
+++ src/backend/routes/adduser.js
@@ -4,26 +4,26 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const mysqlconnection = require("../mysqlconnection");
 const app = express();
-
+var uid;
 sendToMeRouter.post("/adduser", (req, res, next) => {
   //variables are taken from the request and are saved for the sql query
   var fid = req.body.fid;
-  var skill1 = req.body.skill1;
-  var skill2 = req.body.skill2;
-  var skill3 = req.body.skill3;
-  var skill4 = req.body.skill4;
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var uslist = req.body.uslist;
+  adduser(fid, fname, lname);
+  addskills(uid, uslist, fid);
+});
+
+function adduser(fid, fname, lname) {
   //variables needed for the database are added, admin and organization flags are set to a default at the moment
   var sql =
-    "INSERT INTO USERS (fid,admin,oid,skill1,skill2,skill3,skill4) Values('" +
+    "INSERT INTO USERS (fid,admin,oid,fname,lname) Values('" +
     fid +
     "',1,0,'" +
-    skill1 +
+    fname +
     "','" +
-    skill2 +
-    "','" +
-    skill3 +
-    "','" +
-    skill4 +
+    lname +
     "')";
   //query is ran
   mysqlconnection.query(sql, (err) => {
@@ -34,6 +34,44 @@ sendToMeRouter.post("/adduser", (req, res, next) => {
       console.log(err);
     }
   });
-});
+}
+async function getuid(fid) {
+  return new Promise((resolve, reject) => {
+    var sql2 = "SELECT uid FROM USERS WHERE fid='" + fid + "'";
+    mysqlconnection.query(sql2, (err, info) => {
+      if (!err) {
+        console.log(sql2);
+        console.log("user retrived");
+        var uid = info;
+        resolve(uid);
+      } else {
+        console.log(err);
+        reject(err);
+      }
+    });
+  });
+}
+
+async function addskills(uid, uslist, fid) {
+  var i;
+  var res = await getuid(fid);
+  var uid = res[0].uid;
+  for (i = 0; i < uslist.length; i++) {
+    var sql3 =
+      "INSERT INTO USKILLS (uid, sid) Values('" +
+      uid +
+      "','" +
+      uslist[i] +
+      "')";
+    //query is ran
+    mysqlconnection.query(sql3, (err) => {
+      if (!err) {
+        console.log("user skill added to the database");
+      } else {
+        console.log(err);
+      }
+    });
+  }
+}
 
 module.exports = sendToMeRouter;
--- src/components/App/Pages/SignUp/SignUp.js
+++ src/components/App/Pages/SignUp/SignUp.js
@@ -5,32 +5,24 @@ import "./SignUp.css";
 
 import NavBar from "../../../NavBar/NavBar";
 
-import { Select } from 'antd';
+import { Select } from "antd";
 
 const { Option } = Select;
-
-function handleChangeSkills(value) {
-  console.log(`selected ${value}`);
-}
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
     this.signup = this.signup.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.skillChange1 = this.skillChange1.bind(this);
-    this.skillChange2 = this.skillChange2.bind(this);
-    this.skillChange3 = this.skillChange3.bind(this);
-    this.skillChange4 = this.skillChange4.bind(this);
+    this.handleChangeSkills = this.handleChangeSkills.bind(this);
     this.state = {
       email: "",
       password: "",
+      fname: "",
+      lname: "",
       eMessage: "",
-      skill1: "",
-      skill2: "",
-      skill3: "",
-      skill4: "",
       slist: [],
+      uslist: [],
     };
     axios.post("/getskills").then((res) => {
       var slist = res.data;
@@ -51,10 +43,9 @@ class SignUp extends Component {
         axios.post("/adduser", {
           //uid and the skills are sent as part of the database request
           fid: uid,
-          skill1: this.state.skill1,
-          skill2: this.state.skill2,
-          skill3: this.state.skill3,
-          skill4: this.state.skill4,
+          fname: this.state.fname,
+          lname: this.state.lname,
+          uslist: this.state.uslist,
         });
       })
       .catch((error) => {
@@ -66,23 +57,17 @@ class SignUp extends Component {
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
-  //skill change methods are implemented in order to properly implement all four drop down menus
-  skillChange1(event) {
-    this.setState({ skill1: event.target.value });
-  }
-  skillChange2(event) {
-    this.setState({ skill2: event.target.value });
-  }
-  skillChange3(event) {
-    this.setState({ skill3: event.target.value });
-  }
-  skillChange4(event) {
-    this.setState({ skill4: event.target.value });
+  handleChangeSkills(value) {
+    var uslist = value;
+    this.setState({ uslist });
+    console.log(this.state.uslist);
   }
   render() {
     let slist = this.state.slist;
     let optionitems = slist.map((item) => (
-      <option value={item.skills}>{item.skills}</option>
+      <Option value={item.sid} label={item.skills}>
+        <div className="demo-option-label-item">{item.skills}</div>
+      </Option>
     ));
     return (
       <div>
@@ -90,7 +75,8 @@ class SignUp extends Component {
         <div className="signup-page">
           <h1>Create an Account:</h1>
           <form onSubmit={this.signup}>
-            <div class="form-group">  {/* For User First Name */}
+            <div class="form-group">
+              {/* For User First Name */}
               <label for="fname">First Name</label>
               <input
                 value={this.state.fname}
@@ -102,7 +88,8 @@ class SignUp extends Component {
                 required
               />
             </div>
-            <div class="form-group">  {/* For User Last Name */}
+            <div class="form-group">
+              {/* For User Last Name */}
               <label for="lname">Last Name</label>
               <input
                 value={this.state.lname}
@@ -114,7 +101,8 @@ class SignUp extends Component {
                 required
               />
             </div>
-            <div class="form-group">  {/* For User Email */}
+            <div class="form-group">
+              {/* For User Email */}
               <label for="email">Email address</label>
               <input
                 value={this.state.email}
@@ -126,7 +114,8 @@ class SignUp extends Component {
                 required
               />
             </div>
-            <div class="form-group">  {/* For User Password */}
+            <div class="form-group">
+              {/* For User Password */}
               <label for="password">Password</label>
               <input
                 value={this.state.password}
@@ -138,86 +127,19 @@ class SignUp extends Component {
                 required
               />
             </div>
-           {/* <div className="form-group>">
-              <label>Skill #1:</label>
-              <select
-                className="skills-list"
-                value={this.state.skill1}
-                onChange={this.skillChange1}
-              >
-                {optionitems}
-              </select>
-              <label>Skill #2:</label>
-              <select
-                className="skills-list"
-                value={this.state.skill2}
-                onChange={this.skillChange2}
-              >
-                {optionitems}
-              </select>
-              <br />
-              <label>Skill #3:</label>
-              <select
-                className="skills-list"
-                value={this.state.skill3}
-                onChange={this.skillChange3}
-              >
-                {optionitems}
-              </select>
-              <label>Skill #4:</label>
-              <select
-                className="skills-list"
-                value={this.state.skill4}
-                onChange={this.skillChange4}
-              >
-                {optionitems}
-              </select>
-            </div> */}
-            <div> {/* Multiselect input dialogue for preferred Skills */}
+            <div>
+              {" "}
+              {/* Multiselect input dialogue for preferred Skills */}
               <label>Preferred Skills:</label>
               <Select
                 mode="multiple"
-                style={{ width: '60%', marginLeft: '2rem'}}
+                style={{ width: "60%", marginLeft: "2rem" }}
                 placeholder="select 5 skills"
                 //defaultValue={['machine learning']}
-                onChange={handleChangeSkills}
+                onChange={this.handleChangeSkills}
                 optionLabelProp="label"
               >
-                <Option value="machine learning" label="Machine Learning">
-                  <div className="demo-option-label-item">
-                    Machine Learning
-                  </div>
-                </Option>
-                <Option value="front end web dev" label="Front End">
-                  <div className="demo-option-label-item">
-                    Front End Web Development
-                  </div>
-                </Option>
-                <Option value="back end web dev" label="Back End">
-                  <div className="demo-option-label-item">
-                    Back End Web Development
-                  </div>
-                </Option>
-                <Option value="ui ux" label="UI/UX Design">
-                  <div className="demo-option-label-item">
-                    User Interface / User Experience Design
-                  </div>
-                </Option>
-                <Option value="android" label="Android">
-                  <div className="demo-option-label-item">
-                    Android Development
-                  </div>
-                </Option>
-                <Option value="ios" label="iOS">
-                  <div className="demo-option-label-item">
-                    iOS Development
-                  </div>
-                </Option>
-                <Option value="data analysis" label="Data Analysis">
-                  <div className="demo-option-label-item">
-                    Data Analysis
-                  </div>
-                </Option>
+                {optionitems}
               </Select>
             </div>
             <br />
@@ -231,5 +153,4 @@ class SignUp extends Component {
     );
   }
 }
-
 export default SignUp;
