0bf8ae75ced6363dce3c9d675dcd965c6b3b0f76
Anthony <anthony.j.boyko@gmail.com>
2021-03-24 22:23:25 -0700

    added email notification for mentees on acceptance

--- src/backend/routes/sendMenteeAccept.js
+++ src/backend/routes/sendMenteeAccept.js
@@ -5,36 +5,51 @@ const bodyParser = require("body-parser");
 const mysqlconnection = require("../mysqlconnection");
 const app = express();
 var uid;
-sendToMeRouter.post("/addMentee", async (req, res, next) => {
+sendToMeRouter.post("/updateList", async (req, res, next) => {
   //variables are taken from the request and are saved for the sql query
-  console.log("in mentee email setup");
-  res.locals.fname = req.body.fname;
-  res.locals.lname = req.body.lname;
-  res.locals.email = req.body.email;
-  res.locals.subject = "Welcome to Pace B's Mentoring Service";
-  var uid = await getuid(req.body.email);
-  var skillList = await getskills(uid, res);
+  console.log("in mentee email acceptance");
+  console.log(req.body);
+  res.locals.subject = "Meet Your New Mentor";
+  var mentorId = await getuid(req.body.fid);
+  var menteeId = req.body.menteeid;
+  var skillList = await getskills(mentorId);
+  var mentee = await getUser(menteeId);
+  var mentor = await getUser(mentorId);
+  console.log(mentee);
+
+  var mentorFName = mentor[0].fname;
+  var mentorLName = mentor[0].lname;
+  var mentorEmail = mentor[0].email;
+
+  res.locals.mentee_fname = mentee[0].fname;
+  res.locals.mentee_lname = mentee[0].lname;
+  res.locals.mentee_email = mentee[0].email;
 
   var body =
     "Welcome " +
-    req.body.fname +
+    res.locals.mentee_fname +
     " " +
-    req.body.lname +
-    " to Pace B's mentoring service.\n" +
-    "Soon you will be contacted by a mentor that wishes to mentor you in:\n";
+    res.locals.mentee_lname +
+    " to Pace B's mentoring service.\n\n" +
+    "Your mentor is " +
+    mentorFName +
+    " " +
+    mentorLName +
+    " and they are experienced in:\n";
   for (var i = 0; i < skillList.length; i++) {
     body = body + "  " + skillList[i] + "\n";
   }
+  body = body + "\nContact them further at " + mentorEmail + "\n";
   body = body + "\nBest, \n" + "Pace B";
 
   res.locals.body = body;
   next();
 });
 
-async function getuid(email) {
+async function getuid(fid) {
   //query function that retrieves a uid, it needs to be completed before running the rest of the code, signified by the async and await(later)
   return new Promise((resolve, reject) => {
-    var sql2 = "SELECT uid FROM USERS WHERE USERS.email='" + email + "'";
+    var sql2 = "SELECT uid FROM USERS WHERE USERS.fid='" + fid + "'";
     mysqlconnection.query(sql2, (err, info) => {
       if (!err) {
         console.log(sql2);
@@ -55,7 +70,30 @@ async function getuid(email) {
   });
 }
 
-async function getskills(uid, res) {
+async function getUser(uid) {
+  //query function that retrieves a uid, it needs to be completed before running the rest of the code, signified by the async and await(later)
+  return new Promise((resolve, reject) => {
+    var sql2 = "SELECT * FROM USERS WHERE USERS.uid='" + uid + "'";
+    mysqlconnection.query(sql2, (err, info) => {
+      if (!err) {
+        console.log(sql2);
+        console.log(info[0]);
+        //returns with resolve or reject, reject returns an error, where the query fails, and resolve returns the uid, where the query completes
+        resolve(info);
+      } else {
+        console.log(err);
+        reject(err);
+      }
+    });
+  }).catch((error) => {
+    console.log("hit error");
+    const eMessage = error.message;
+    this.setState({ eMessage });
+    console.log(error);
+  });
+}
+
+async function getskills(uid) {
   return new Promise((resolve, reject) => {
     //sql query is a combination of the technology and user skill tables, so the skills are filtered to just show those of an individual user
     var sql =
--- src/backend/server.js
+++ src/backend/server.js
@@ -12,19 +12,19 @@ app.use(express.json());
 //mentor post request is unhandled
 app.post("/addMentor", require("./routes/addMentor"));
 app.post("/findMatch", require("./routes/findMatch"));
-// app.post(
-//   "/findMatch",
-//   require("./routes/findMatch"),
-//   require("./routes/sendMail")
-// );
-//app.post("/findMatch",);
+
 app.post("/getProfile", require("./routes/getProfile"));
 app.post("/getSkills", require("./routes/getSkills"));
 app.post("/mentor", require("./routes/cookies"));
 app.post("/getUserSkills", require("./routes/getUserSkills"));
 app.post("/getUnmatchedMentees", require("./routes/getUnmatchedMentees"));
 app.post("/getMatchedMentees", require("./routes/getMatchedMentees"));
-app.post("/updateList", require("./routes/updateList"));
+app.post(
+  "/updateList",
+  require("./routes/updateList"),
+  require("./routes/sendMenteeAccept"),
+  require("./routes/sendMail")
+);
 app.post(
   "/addMentee",
   require("./routes/addMentee"),
