e2697e0faa4e630972cb42613b56635f3d47d0cf
cpasteris <cpasteris@shaw.ca>
2021-03-26 00:38:02 -0600

    invite functionality completed

--- src/backend/routes/addMentor.js
+++ src/backend/routes/addMentor.js
@@ -10,22 +10,44 @@ sendToMeRouter.post("/addMentor", async (req, res, next) => {
   var lname = req.body.lname;
   var email = req.body.email;
   var fid = req.body.fid;
-  await adduser(fid, fname, lname, email);
+  var oid = req.body.oid;
+  adduser(fid, fname, lname, email, oid);
+  dropInvite(email);
 });
 
-async function adduser(fid, fname, lname, email) {
+async function adduser(fid, fname, lname, email, oid) {
   return new Promise(async (resolve, reject) => {
-    //variables needed for the database are added, admin and organization flags are set to a default at the moment
+    //variables needed for the database are added, admin flag is set to a default at the moment
     var sql =
       "INSERT INTO USERS (fid,admin,oid,fname,lname,email,utype) Values('" +
       fid +
-      "',1,1,?,?,?,1)";
+      "',1," +
+      oid +
+      ",?,?,?,1)";
     console.log("mentor is being added");
     //query is ran
     mysqlconnection.query(sql, [fname, lname, email], (err) => {
       if (!err) {
         console.log("account added to the database");
         console.log(sql);
+        resolve();
+      } else {
+        console.log(err);
+        reject(err);
+      }
+    });
+  });
+}
+async function dropInvite(email) {
+  return new Promise(async (resolve, reject) => {
+    //after being used, the invite related to the spescific email is removed from the database
+    var sql2 = "DELETE FROM INVITES WHERE email=?";
+    console.log("mentor is being added");
+    //query is ran
+    mysqlconnection.query(sql2, [email], (err) => {
+      if (!err) {
+        console.log("invite removed from the database");
+        console.log(sql2);
         resolve();
       } else {
         console.log(err);
--- src/components/App/Pages/SignUp/SignUp.js
+++ src/components/App/Pages/SignUp/SignUp.js
@@ -14,42 +14,74 @@ class SignUp extends Component {
     super(props);
     this.signup = this.signup.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.getInviteList = this.getInviteList.bind(this);
     this.state = {
       email: "",
       password: "",
       fname: "",
       lname: "",
       eMessage: "",
+      emailList: [],
+      inviteList: [],
     };
+    this.getInviteList();
   }
   componentWillMount() {
     this.mounted = true;
   }
-  signup(e) {
-    e.preventDefault();
-    fire
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((u) => {
-        message.success("Successfully Signed Up!");
-        this.props.history.push("/mentor");
-        var uid = u.user.uid;
-        //request to send user information to the database
-        axios.post("/addMentor", {
-          //uid, first name, last name and user skills are sent as part of the database request
-          fid: uid,
-          fname: this.state.fname,
-          lname: this.state.lname,
-          email: this.state.email,
-          uslist: this.state.uslist,
-        });
+  getInviteList() {
+    //gets the list of invites
+    axios
+      .post("/getInvites", {
+        //request type is sent as part of the database request
+        reqtype: 1,
       })
-      .catch((error) => {
-        //logs an error if one occurs, and displays it to the user
-        const eMessage = error.message;
-        this.setState({ eMessage });
-        console.log(error);
+      .then((res) => {
+        var inviteList = res.data;
+        var emailList = res.data.map((item) => item.email);
+        console.log(inviteList);
+        console.log(emailList);
+        this.setState({ inviteList });
+        this.setState({ emailList });
+        //list of valid emails are saved, as well as the database response
       });
+  }
+  signup(e) {
+    //if account creation needs to be tested without invites comment out the if else statment that checks the invite list
+    //index of the email is checked based on the submitted email, if the email is not in the email list, and error is shown saying that the email is invaild, and has not been invited
+    //if the email is in the list, the emails respective oid is retrived, and the signup process starts
+    e.preventDefault();
+    var inviteIndex = this.state.emailList.indexOf(this.state.email);
+    if (inviteIndex > -1) {
+      var oid = this.state.inviteList[inviteIndex].oid;
+      //account is created in firebase using its respective commands
+      fire
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((u) => {
+          message.success("Successfully Signed Up!");
+          this.props.history.push("/mentor");
+          var uid = u.user.uid;
+          //request to send user information to the database
+          axios.post("/addMentor", {
+            //uid, first name, last name and organization id are sent as part of the database request
+            fid: uid,
+            fname: this.state.fname,
+            lname: this.state.lname,
+            email: this.state.email,
+            oid: oid,
+          });
+        })
+        .catch((error) => {
+          //logs an error if one occurs, and displays it to the user
+          const eMessage = error.message;
+          this.setState({ eMessage });
+          console.log(error);
+        });
+    } else {
+      //if the email is not valid, error is displayed
+      this.setState({ eMessage: "Email address is not valid" });
+    }
   }
   handleChange(e) {
     //saves text box contents into their proper variables
